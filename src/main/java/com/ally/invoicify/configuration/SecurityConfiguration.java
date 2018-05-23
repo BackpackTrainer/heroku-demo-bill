178bc775bd8411cffd36a3d48f3021bf4a5635ca
Eric Schwartz <erics273@gmail.com>
2018-05-23 12:02:01 -0400

    Initial commit

---
+++ src/main/java/com/ally/invoicify/configuration/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.ally.invoicify.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.ally.invoicify.services.InvoicifyUserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	private InvoicifyUserDetailsService userDetailsService;
+	
+	public SecurityConfiguration(InvoicifyUserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http	
+			.authorizeRequests()
+				.antMatchers("/", "/css/**", "/js/**", "/signup").permitAll()
+				.antMatchers("/invoices/**").hasAnyRole("ADMIN", "ACCOUNTANT")
+				.antMatchers("/billing-records/**").hasAnyRole("ADMIN", "CLERK")
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+			.and()
+			.formLogin();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	public UserDetailsService userDetailsService() {
+		return userDetailsService;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
@@ -0,0 +1,47 @@
+package com.ally.invoicify.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ally.invoicify.repositories.BillingRecordRepository;
+import com.ally.invoicify.repositories.CompanyRepository;
+
+@Controller
+@RequestMapping("/billing-records")
+public class BillingRecordController {
+	
+	private BillingRecordRepository recordRepository;
+	private CompanyRepository companyRepository;
+
+	public BillingRecordController(BillingRecordRepository recordRepository, CompanyRepository companyRepository) {
+		this.recordRepository = recordRepository;
+		this.companyRepository = companyRepository;
+	}
+
+	@GetMapping("")
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView("billing-records/list");
+		mv.addObject("records", recordRepository.findAll());
+		mv.addObject("companies", companyRepository.findAll());
+		return mv;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
